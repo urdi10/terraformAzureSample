aa93545e960c0b5ea07f2ed962899decc3b3656a
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2018-04-13 20:25:14 +0200

    virtual machine

--- test.tf
+++ test.tf
@@ -112,3 +112,48 @@ resource "azurerm_storage_account" "mystorageaccount" {
         environment = "Terraform Demo"
     }
 }
+
+
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "${azurerm_resource_group.myterraformgroup.name}-vm"
+    location              = "${azurerm_resource_group.myterraformgroup.location}"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04.0-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "${azurerm_resource_group.myterraformgroup.name}-vm"
+        admin_username = "azureuser"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "/home/azureuser/.ssh/authorized_keys"
+            key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwyhN7++iT/2E3/D+AcCEUjN+aZEDnPmnL+nhROEfXdx9b2OVYekwO48k4/AbNQm5S6ueceExwJXTSe//KpUfX5zd8bx82LcCb36Rm9KpGuy0tyh5cMWU9swxEP0KDd0lySrtvHJF0tuK/CWUl3FOH8HIDqMWR+Ks5dD1EEymv8kfGa1NXoXQZS1xKiqMQP7hCsmVQeJvoLOk3XYObFVBsUOAXEPL8usOd5Vte/z7eW3JVhnYKcqk5aHc7SsV9O/uuzhMM89t9GBmC7vwJGKuESwX8fus0UOk+eGnwEEHxa6qp/zTQuQ8E9WNl/wcVRy87vfoIhuA555Vi4BdjEp67 jjcanada@ual.es"
+        }
+    }
+
+    boot_diagnostics {
+        enabled     = "true"
+        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
