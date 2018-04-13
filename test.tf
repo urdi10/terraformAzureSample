ec22da0f7a43aaa02b52cef881b9e595f78dcd53
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2018-04-13 20:07:00 +0200

    subnet and public ip

--- test.tf
+++ test.tf
@@ -11,10 +11,33 @@ resource "azurerm_resource_group" "myterraformgroup" {
 }
 
 resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
+    name                = "${azurerm_resource_group.myterraformgroup.name}-net"
     address_space       = ["10.0.0.0/16"]
     location            = "${azurerm_resource_group.myterraformgroup.location}"
     resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "${azurerm_resource_group.myterraformgroup.name}-subnet"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.2.0/24"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+
+}
+
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "${azurerm_virtual_network.myterraformnetwork.name}-publicIP"
+    location                     = "${azurerm_resource_group.myterraformgroup.location}"
+    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+    public_ip_address_allocation = "dynamic"
 
     tags {
         environment = "Terraform Demo"
