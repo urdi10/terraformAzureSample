3ee36a3deb2871aee17f6a8a17c283fc96cd1978
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2018-04-13 20:20:34 +0200

    cuenta almacenamiento

--- test.tf
+++ test.tf
@@ -91,3 +91,24 @@ resource "azurerm_network_interface" "myterraformnic" {
         environment = "Terraform Demo"
     }
 }
+
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = "${azurerm_resource_group.myterraformgroup.name}"
+    }
+
+    byte_length = 8
+}
+
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                = "diag${random_id.randomId.hex}"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    location            = "${azurerm_resource_group.myterraformgroup.location}"
+    account_replication_type = "LRS"
+    account_tier = "Standard"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
