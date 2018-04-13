b943efaa84d19bb1764d097ab1be934f91b2acb9
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2018-04-13 20:12:16 +0200

    security group

--- test.tf
+++ test.tf
@@ -39,3 +39,37 @@ resource "azurerm_public_ip" "myterraformpublicip" {
         environment = "Terraform Demo"
     }
 }
+
+
+resource "azurerm_network_security_group" "temyterraformpublicipnsg" {
+    name                = "${azurerm_virtual_network.myterraformnetwork.name}-networkSecurityGroup"
+    location            = "${azurerm_resource_group.myterraformgroup.location}"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    ;
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
